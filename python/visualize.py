52dcdbf9790f4430b7b09e67a1793bbb482ae9b7
dewaeq <dewaeq@protonmail.com>
2025-08-31 13:42:13 +0200

    control car from visualize node

--- python/visualize.py
+++ python/visualize.py
@@ -5,21 +5,16 @@ from math import inf, radians, cos, sin
 
 from messages.LaserScan import LaserScan
 from messages.ScanPoint import ScanPoint
+from messages import SteerCommand
 
 
 SIZE = 600
 display = pygame.display.set_mode((600, 600))
 
 msg: LaserScan | None = None
+last_cmd = SteerCommand()
 
-def loop():
-    lc.handle()
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-
+def draw():
     if msg == None:
         return
 
@@ -46,6 +41,39 @@ def loop():
 
     pygame.draw.circle(display, (255, 255, 255), (SIZE/2, SIZE/2), 5)
     pygame.display.update()
+
+def steer():
+    global last_cmd
+
+    cmd = SteerCommand()
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP]:
+        cmd.speed = 100
+    if keys[pygame.K_DOWN]:
+        cmd.speed = -100
+    if keys[pygame.K_LEFT]:
+        cmd.turn = -1
+    if keys[pygame.K_RIGHT]:
+        cmd.turn = 1
+
+    if cmd.turn != last_cmd.turn or cmd.speed != last_cmd.speed:
+        lc.publish("steer_command", cmd.encode())
+        last_cmd = cmd
+
+
+def loop():
+    # lc.handle()
+    lc.handle_timeout(5)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    draw()
+    steer()
+
 
 
 def handler(channel, data):
