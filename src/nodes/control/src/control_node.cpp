978301b852a35152c6c35a054595c606a0b35ccb
dewaeq <dewaeq@protonmail.com>
2026-01-13 17:25:08 +0100

    simplified control node

--- src/nodes/control/src/control_node.cpp
+++ src/nodes/control/src/control_node.cpp
@@ -1,6 +1,5 @@
 #include "control/control_node.hpp"
 #include "scylla_msgs/drive_command_t.hpp"
-#include <chrono>
 
 ControlNode::ControlNode() : LcmNode("control_node") {
   driver_.set_steering(0);
@@ -13,21 +12,13 @@ void ControlNode::handle_command(const scylla_msgs::drive_command_t *msg) {
   driver_.set_throttle(msg->throttle);
   driver_.set_steering(msg->steering);
 
-  auto now = std::chrono::steady_clock::now();
-  last_cmd_time_ms_ = std::chrono::duration_cast<std::chrono::milliseconds>(
-                          now.time_since_epoch())
-                          .count();
+  last_cmd_time_ = now();
 }
 
 void ControlNode::update() {
-  auto now = std::chrono::steady_clock::now();
-  int64_t current_ms = std::chrono::duration_cast<std::chrono::milliseconds>(
-                           now.time_since_epoch())
-                           .count();
-
   // SAFETY CHECK:
   // stop if no command for 500ms
-  if ((current_ms - last_cmd_time_ms_) > 500) {
+  if ((now() - last_cmd_time_) > 0.2) {
     driver_.set_steering(0);
     driver_.set_throttle(0);
   }
